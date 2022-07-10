13fb3c553a55a4256ccb6d20cb2e1ec7f7632f94
mahmod1333 <mahmod.ayman.okasha@gmail.com>
2022-07-10 04:48:26 +0200

    add a front end quiz app

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const quiz = [
+    {
+        question: "What is the capital of the United States?",
+        answers: ["Washington", "New York", "Los Angeles", "Chicago"],
+        correctAnswer: "Washington",
+    },
+    {
+        question: "What is the capital of the United Kingdom?",
+        answers: ["London", "Paris", "Berlin", "Rome"],
+        correctAnswer: "London",
+    },
+    {
+        question: "how old is the sun?",
+        answers: ["1 million years old", 
+        "1 billion years old", 
+        "1 trillion years old", 
+        "1 quadrillion years old"
+    ],
+        correctAnswer: "1 billion years old",
+    },   {
+        question: "what is ui in react?",
+        answers: ["user interface", "user input", "user interface", "user interface"],
+        correctAnswer: "user interface",
+    },   {
+        question: "what is state in react?",
+        answers: ["tool","object","variable","property"],
+        correctAnswer: "property",
+    }];
+
+      
